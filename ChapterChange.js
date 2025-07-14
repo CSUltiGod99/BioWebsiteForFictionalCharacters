8fbbeb3de1fc3bccfe852c7bc8463ac78f5f5cf0
Blane <80710483+CSUltiGod99@users.noreply.github.com>
2025-07-14 19:44:43 +0100

    Update ChapterChange.js

--- ChapterChange.js
+++ ChapterChange.js
@@ -1,9 +1,16 @@
 
-function ChangeChapter()
+function ChangeChapters()
 {
+    let const chapterswitch[4] = {Pg 1, Pg 2, Pg 3, Pg 4};
+
     
+    switch 
+
+    window.location.replace = "Link-3";
     
 }
+
+
 
 
 
