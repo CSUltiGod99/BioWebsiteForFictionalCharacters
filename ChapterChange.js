72a0caf6866a73c4073ffaa6f1abad6f95193c8b
Blane <80710483+CSUltiGod99@users.noreply.github.com>
2025-08-11 00:24:56 +0100

    Update ChapterChange.js

--- ChapterChange.js
+++ ChapterChange.js
@@ -1,4 +1,13 @@
-
+<!DOCTYPE html>
+<html>
+<head></head>
+<title></title>
+<body>
+    <div>
+        <a href="WebPage1" target="WebPage_2.html">Next-Page</a>
+        <h3>New Web Page</h3>
+    </div>
+<script>
 function ChangeChapters()
 {
     let const chapterswitch[4] = {Pg 1, Pg 2, Pg 3, Pg 4};
@@ -12,5 +21,8 @@ function ChangeChapters()
 
 
 
+    </script>
 
+    </body>
+    </html>
 
